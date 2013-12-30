d0e603bd7e88b0d087df4b53ab082c64ad3c0a9e
Chelsea <Chelsea@chelsea-orcutts-macbook-pro.local>
2013-12-29 21:17:18 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '2629c9504fb2c0f0f3bebfadb8fcf708dc1f6feac22331a72d18b41c6a66bd0c23d091e0f766aed7f68367d95948ce0f531198c5f92df6aa4bf00390aab722ef'
